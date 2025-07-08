e852e3114cdf2e50a01770a4659ce9c4ec30da2b
samanyuahuja <samanyuahuja@gmail.com>
2025-07-08 21:37:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -132,5 +132,5 @@ def result():
                            shap_plot=shap_plot_path,
                            pdp_plot=pdp_plot_path)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
